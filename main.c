5892d89f8e397c03f892ebc717c7362300357e7d
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-12-07 11:46:37 +0100

    add clock time for benchamkrs

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "calendar.h"
 #include "fibonacci.c"
+#include <time.h>
 int main()
 {
     // int year = 2025;
@@ -9,12 +10,17 @@ int main()
 
     // printf("Jour dans semaine : %d\n", jour_dans_semaine(day, month, year));
 
-
-    //fibonacci
-    int n=12;
-    int res=fibonacci_rec(n);
-    printf("fibonacci de %d is %d \n",n,res);
-
-
+    // fibonacci
+    int n = 45;
+    clock_t start=clock();
+    printf("fibonacci_iter de %d is %d \n", n, fibonacci_iter(n));
+    clock_t end=clock();
+    printf("clock for the iterfative version is : (%ld - %ld)=%ld \n",start,end,end-start);
+    
+    
+    clock_t startr=clock();
+    printf("fibonacci_rec de %d is %d \n", n, fibonacci_rec(n));
+    clock_t endr=clock();
+    printf("clock for the rec version is : (%ld - %ld)=%ld \n",startr,endr,endr-startr);
     return 0;
 }
